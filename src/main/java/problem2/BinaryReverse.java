abce854bee8e893c765d9ae0e98bb8a6195936c7
Puje <puje017@gmail.com>
2021-03-23 02:58:55 -0500

    add screenshots

--- src/main/java/problem2/BinaryReverse.java
+++ src/main/java/problem2/BinaryReverse.java
@@ -12,26 +12,19 @@ public class BinaryReverse {
 
         int num = Integer.parseInt(str);
         String s = Integer.toBinaryString(num);
-
         String[] arr = new String[8];
-
         Arrays.fill(arr, "0");
-
         for (int i = s.length() - 1, j = 0; i >= 0; i--, j++) {
             char ch = ' ';
             ch = s.charAt(i);
-
             arr[j] = Character.toString(ch);
         }
-
         String result = "";
         for (String i : arr) {
             result += i;
 
         }
-
         int decimal = Integer.parseInt(result, 2);
-
         return Integer.toString(decimal);
     }
 
